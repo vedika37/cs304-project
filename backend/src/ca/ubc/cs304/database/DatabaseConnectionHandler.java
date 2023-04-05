1a0aa68013910432ff6a30e8f46d3ee93203d338
Vedika Seth <vedika07@students.cs.ubc.ca>
2023-04-05 16:57:28 -0700

    changed bestPerformingTeam to return 1 team

--- backend/src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
+++ backend/src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
@@ -337,7 +337,7 @@ public class DatabaseConnectionHandler {
     }
 
     // TODO performace points?? also need to add custom obj to frontend to display
-    // projection query order by rank
+    // projection query
     public ArrayList<PlayerHasRankingIsInTeamFollowsModel> getRankingsBySeason(String givenSeason) {
         ArrayList<PlayerHasRankingIsInTeamFollowsModel> result = new ArrayList<>();
         try {
@@ -632,26 +632,15 @@ public class DatabaseConnectionHandler {
         TeamModel result = null;
 
         try {
-//            alternative
-//            SELECT t.TYPE,TEAMNAME,t.DIVISION, AVG(Pe.performancePoints)
-//            FROM   playerHasRankingIsInTeamFollows P, Team T, displaysPerformance Pe
-//            WHERE  P.teamName = T.name AND P.playerID = Pe.playerID
-//            GROUP BY t.TYPE, TEAMNAME, t.DIVISION
-//            HAVING avg(Pe.performancePoints) >= all (SELECT AVG(Pe.performancePoints)
-//                    FROM   playerHasRankingIsInTeamFollows P, Team T, displaysPerformance Pe
-//                    WHERE  P.teamName = T.name AND P.playerID = Pe.playerID
-//                    GROUP BY teamName)
-//            System.out.println(givenTeamName);
             PreparedStatement ps = connection.prepareStatement("SELECT teamName, AVG(Pe.performancePoints)\n" +
                     "    FROM   playerHasRankingIsInTeamFollows P, Team T, displaysPerformance Pe\n" +
-                    "    WHERE  P.teamName = T.name AND P.playerID = Pe.playerID\n" +
+                    "    WHERE  rownum = 1 AND P.teamName = T.name AND P.playerID = Pe.playerID\n" +
                     "    GROUP BY teamName\n" +
                     "    HAVING avg(Pe.performancePoints) >= all (SELECT AVG(Pe.performancePoints)\n" +
                     "    FROM   playerHasRankingIsInTeamFollows P, Team T, displaysPerformance Pe\n" +
                     "    WHERE  P.teamName = T.name AND P.playerID = Pe.playerID\n" +
                     "    GROUP BY teamName)");
-//            ps.setString(1, givenTeamName);
-//            ps.setString(2, givenTeamName);
+
             ResultSet rs = ps.executeQuery();
 
             if(rs.next()) {
